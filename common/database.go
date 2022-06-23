a92e0e413b7e7b59ac25786193571a60289f6425
Orange <2807282372@qq.com>
2022-06-23 21:48:35 +0800

    :wrench: 新增从 yaml 文件中读取配置信息

--- common/database.go
+++ common/database.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"orangezoom.cn/ginessential/model"
@@ -10,12 +11,12 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	host := "127.0.0.1"
-	port := "3306"
-	database := "essential"
-	username := "root"
-	password := "root"
-	charset := "utf8"
+	host := viper.GetString("database.host")
+	port := viper.GetString("database.port")
+	database := viper.GetString("database.database")
+	username := viper.GetString("database.username")
+	password := viper.GetString("database.password")
+	charset := viper.GetString("database.charset")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
 		username,
 		password,
--- main.go
+++ main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"log"
 	"orangezoom.cn/ginessential/common"
 	_ "orangezoom.cn/ginessential/model"
+	"os"
 )
 
 func main() {
+	InitConfig()
+
+	log.Println(viper.GetString("database.host"))
 	common.InitDB()
 	r := gin.Default()
 	r = CollectRoute(r)
-	panic(r.Run())
+
+	port := viper.GetString("server.port")
+
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+	panic(r.Run(":" + port))
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(workDir + "\\config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
